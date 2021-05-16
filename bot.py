b3529e907d7acc0d1135dcdfd5abfaa76c801f76
epic_developer <simonglobdob@gmail.com>
2021-05-16 12:27:41 +0200

    initial commit

---
+++ bot.py
@@ -0,0 +1,66 @@
+from hata import Client
+import config
+import PIL.Image
+import requests
+import log
+
+DN = Client(config.bot_token)
+
+ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", "/", "!", ",", "."]
+
+def resize_image(image, new_width=100):
+    width, height = image.size
+    ratio = height / width
+    new_height = int(new_width * ratio)
+    return image.resize((new_width, new_height)), new_width
+
+def grayify(image):
+    return image.convert("L")
+
+def img_to_ascii(url):
+    img = PIL.Image.open(requests.get(url, stream=True).raw)
+    img = grayify(img)
+    img, new_width = resize_image(img)
+
+    pixels = img.getdata()
+    chars = "".join([ASCII_CHARS[pixel//20] for pixel in pixels])
+
+    chars_len = len(chars)
+    return "\n".join(chars[i:(i+new_width)] for i in range(0, chars_len, new_width))
+
+
+@DN.events
+async def ready(ctx):
+    log.info("Logged in.")
+
+@DN.events
+async def message_create(ctx, message):
+    if message.author.is_bot:
+        return
+
+    if message.content.lower().startswith("+convert"):
+        if message.attachments:
+            ascii = img_to_ascii(message.attachments[0].proxy_url)
+            
+            if len(ascii) <= 2000:
+                await ctx.message_create(message.channel, "`{}`".format(ascii))
+                return
+
+            await ctx.message_create(message.channel, "Sending ascii... ({} characters).".format(len(ascii)))
+
+            while True:
+                if len(ascii) >= 1010:
+                    await ctx.message_create(message.channel, "`{}`".format(ascii[:1010]))
+
+                    ascii = ascii[1010:]
+                else:
+                    break
+
+                continue
+
+            return
+
+    return
+
+
+DN.start()
---
+++ config.example.py
@@ -0,0 +1,1 @@
+bot_token = "deez nuts"
